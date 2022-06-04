c523cf74f368926b21448c88ecad84edd0aa09e8
Ty <everton_915@yahoo.com>
2022-06-04 17:47:28 -0600

    removed some unnecessary code

--- src/components/List-Card.tsx
+++ src/components/List-Card.tsx
@@ -1,25 +1,9 @@
-import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 type postProps = { item: any }
 
 function ListCard(props: postProps) {
-  // below is an anonymous function | defining a function, but not giving it a name
-  // also known as inlining a function
-
-  const [postData, setPostData] = useState<any>([])
-
   const navigate = useNavigate()
-
-  const toIndividualPost = () => {
-    // axios
-    //   .get(`https://jsonplaceholder.typicode.com/posts/${props.item.id}`)
-    //   .then((response) => {
-    //     setPostData(response.data)
-    //   })
-  }
-
-  //   useEffect(() => {}, [])
 
   return (
     <div
@@ -36,10 +20,9 @@ function ListCard(props: postProps) {
           width: 500,
           color: 'blue',
           fontWeight: 'bold',
-          textDecoration: 'underline',
         }}
       >
-        {props.item.id}
+        {props.item.title}
       </div>
       <div>{props.item.body}</div>
       <button onClick={() => navigate(`/${props.item.id}`)}>
